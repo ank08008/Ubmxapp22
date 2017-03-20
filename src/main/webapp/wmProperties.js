8b79e567aa8f7a8c80337069957e39781e8e0302
root <root@e98dc42d4a08>
2017-03-20 08:29:38 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Ubmxapp22",
+  "homePage" : "Main",
+  "name" : "Ubmxapp22",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
